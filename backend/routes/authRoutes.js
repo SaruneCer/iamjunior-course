95d824c87624d716d469810e7ec95b8792969c0e
s.cernikeviciene <sarune.ku@gmail.com>
2024-08-14 22:27:23 +0300

    Implemented authentication

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const User = require("../schemas/User")
+const router = express.Router();
+const {generateToken} = require("../utils/password")
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = req.body;
+    const existingUser = await User.findOne({ email: user.email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User exists" });
+    }
+      const newUser = new User(user);
+      await newUser.save();
+    return res.status.apply(200).json({ message: "User registered successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error registering user", error: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = req.body
+
+        const existingUser = await User.findOne({ email: user.email })
+        if (!existingUser) {
+            return res.status(400).json({ message: "User does not exist" });
+        }
+        
+        const isCorrectPassword = await existingUser.isCorrectPassword(user.password)
+        if (!isCorrectPassword) {
+            return res.status(404).json ({message: "Invalid password"})
+        }
+
+        const token = generateToken ({id: existingUser._id})
+
+        return res.json ({message: "Logged in", token, existingUser})
+    }
+    catch (error){}
+})
+
+module.exports = router;
--- backend/schemas/Business.js
+++ backend/schemas/Business.js
@@ -1,55 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ImageSchema = new mongoose.Schema({
-  url: {
-    type: String,
-    required: [true, 'Image URL is required.'],
-    validate: {
-      validator: function (url) {
-        return /^(http|https):\/\/[^\s$.?#].[^\s]*$/.test(url);
+const BusinessSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Business name is required."],
+    },
+    about: {
+      type: String,
+      default: "",
+    },
+    address: {
+      type: String,
+      required: [true, "Business address is required."],
+    },
+    category: {
+      type: String,
+      required: [true, "Business category is required."],
+      trim: true,
+    },
+    contactPerson: {
+      type: String,
+      required: [true, "Contact person is required."],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      match: [/.+\@.+\..+/, "Please provide a valid email address."],
+      trim: true,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
       },
-      message: (props) => `${props.value} is not a valid URL!`
-    }
+    ],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
   }
-});
+);
 
-const BusinessSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Business name is required.'],
-    trim: true
-  },
-  about: {
-    type: String,
-    required: [true, 'Business description is required.'],
-    trim: true
-  },
-  address: {
-    type: String,
-    required: [true, 'Business address is required.'],
-    trim: true
-  },
-  category: {
-    type: String,
-    required: [true, 'Business category is required.'],
-    trim: true
-  },
-  contactPerson: {
-    type: String,
-    required: [true, 'Contact person is required.'],
-    trim: true
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required.'],
-    match: [/.+\@.+\..+/, 'Please provide a valid email address.'],
-    trim: true
-  },
-  images: [ImageSchema]
-}, {
-  versionKey: false,
-  timestamps: true
-});
-
-const Business = mongoose.model('Business', BusinessSchema);
+const Business = mongoose.model("Business", BusinessSchema);
 module.exports = Business;
